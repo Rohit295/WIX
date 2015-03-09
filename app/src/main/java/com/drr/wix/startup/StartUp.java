6540e46a63d65b04cb5486bbd1851aae23a0e33d
Rohit295 <rohit295@gmail.com>
2015-03-09 22:43:29 +0530

    Initial Version

---
+++ app/src/main/java/com/drr/wix/startup/StartUp.java
@@ -0,0 +1,6 @@
+package com.drr.wix.startup;
+/**
+ * Created by rohitman on 3/9/2015.
+ */
+public class StartUp {
+}
---
+++ app/src/main/java/com/drr/wix/tracker/TrackerHandler.java
@@ -0,0 +1,9 @@
+package com.drr.wix.tracker;
+
+import android.os.Handler;
+
+/**
+ * Created by rohitman on 3/9/2015.
+ */
+public class TrackerHandler extends Handler {
+}
